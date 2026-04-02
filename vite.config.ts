b1099dac456578ae9cbab0ffb5010cfbdbedd3ba
alimkhodr <ali.m.khodr@gmail.com>
2026-04-01 22:07:25 -0300

    feat: add reka ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import RekaResolver from 'reka-ui/resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      resolvers: [
+        RekaResolver()
+      ],
+    }),
+  ],
 })
